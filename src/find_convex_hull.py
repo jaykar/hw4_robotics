63c42566a9f7d16d5078a248f6992add9e888dd8
jakenayeck <jan2150@columbia.edu>
2015-11-18 16:40:46 -0500

    working convex hull

--- src/find_convex_hull.py
+++ src/find_convex_hull.py
@@ -1,12 +1,14 @@
+# -*- coding: utf-8 -*-
 import math
 
 def find_convex_hull(pts_array):
+
     num_pts = len(pts_array);
     
     # find the rightmost, lowest point, label it P0
     sorted_pts = sorted(pts_array, key=lambda element: (element[0], -element[1]))
     P0 = sorted_pts.pop()
-    print(P0)
+    print P0
     P0_x = P0[0];
     P0_y = P0[1];
     
@@ -17,7 +19,7 @@ def find_convex_hull(pts_array):
     for i in range(num_pts-1):
         x = pts_array[i][0]
         y = pts_array[i][1]
-        angle = 0;
+        angle = 0.0;
         x_diff = x - P0_x
         y_diff = y - P0_y
         angle = math.degrees(math.atan2(y_diff, x_diff));
@@ -27,7 +29,7 @@ def find_convex_hull(pts_array):
         pt_info = (round(angle, 3), round(dist,3), pts_array[i])
         sort_array.append(pt_info)
     sorted_pts = sorted(sort_array, key=lambda element: (element[0], element[1]))
-    print(sorted_pts)
+    print sorted_pts
     # Push the points labeled PN−1 and P0 onto a stack. T
     # these points are guaranteed to be on the Convex Hull
     pt_stack = []
@@ -43,7 +45,7 @@ def find_convex_hull(pts_array):
         P_i = sorted_pts[i][2];
         c = pt_stack.pop();
         d = pt_stack.pop();
-        print(P_i, c, d)
+        print P_i, c, d
         pt_stack.append(d);
         pt_stack.append(c);
         # find the line formed by these two points and see if the point Pi is
@@ -52,7 +54,10 @@ def find_convex_hull(pts_array):
         if c[0] != d[0]: # not a vertical line in conventional xy plane
             m = (c[1] - d[1])/(c[0] - d[0])
             b = c[1] - m*(c[0])
+            
+            print "c,d", c, d
             print(m, b)
+
             if m == 0: 
                 if (P_i[0] < min(c[0], d[0])):
                     is_to_the_left = True
@@ -63,18 +68,18 @@ def find_convex_hull(pts_array):
         else: 
             if P_i[0] < c[0]:
                 is_to_the_left = True
-        print(is_to_the_left)
-        if (is_to_the_left):
+        print is_to_the_left
+        if is_to_the_left:
             pt_stack.append(P_i);
             i += 1;
         else:
             pt_stack.pop();
-        print("revised stack:")
-        print(pt_stack)
-        print('\n')
+        print "revised stack:"
+        print pt_stack
+        print '\n'
     return pt_stack;
 
-pts = [[0, 1], [1, 5], [2, 3], [2, 3], [3, 5], [3, 2], [4, 2], [6, 3]];                      
+pts = [[0.0, 1.0], [1.0, 5.0], [2.0, 3.0], [2.0, 3.0], [3.0, 5.0], [3.0, 2.0], [4.0, 2.0], [6.0, 3.0]]
 hull = find_convex_hull(pts);      
 print(hull)       
          
