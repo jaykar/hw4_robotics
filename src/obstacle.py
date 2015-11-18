5bff554ea34a17fdc92415af5bf4f16dbeb9b602
jakenayeck <jan2150@columbia.edu>
2015-11-17 23:48:06 -0500

    working version

--- src/obstacle.py
+++ src/obstacle.py
@@ -3,27 +3,123 @@ import cv2
 from scipy.spatial import ConvexHull
 import matplotlib.pyplot as plt
 
+class Line(object):
+    def __init__(self, start, end):
+        self.x0 = start[0]
+        self.y0 = start[1]
+        self.x1 = end[0]
+        self.y1 = end[1]
+        self.constant, self.m  = self.gradient()
+        self.b  = self.y_intercept()
+
+        self.A = self.y1 - self.y0
+        self.B = self.x0 - self.x1
+        self.C = self.A*self.x0 + self.B*self.y0
+
+    def line_intersection(self,line):
+        det = self.A * line.B - line.A * self.B
+        xpts = [line.x0, line.x1]
+        ypts = [line.y0, line.y1]
+        min_x, min_y = min(xpts), min(ypts)
+        max_x, max_y = max(xpts), max(ypts)
+
+        min_x = round(min_x, 3)
+        max_x = round(max_x, 3)
+        min_y = round(min_y, 3)
+        max_y = round(max_y, 3)
+
+
+        xpts = [self.x0, self.x1]
+        ypts = [self.y0, self.y1]
+        min_x_p, min_y_p = min(xpts), min(ypts)
+        max_x_p, max_y_p = max(xpts), max(ypts)
+
+        min_pt_x = round(min_x_p, 3)
+        max_pt_x = round(max_x_p, 3)
+        min_pt_y = round(min_y_p, 3)
+        max_pt_y = round(max_y_p, 3)
+
+        if det == 0:
+            return False
+        else:
+            x = round((line.B * self.C - self.B * line.C)/det, 3)
+            y = round((self.A * line.C - line.A * self.C)/det, 3)
+            
+            #print max_y - y
+            #print "intersection x, y", x, y
+            #print "self", (self.x0,  self.y0), (self.x1, self.y1)
+            #print "line", (line.x0,  line.y0), (line.x1, line.y1)
+            #print "min_x, max_x", min_x, max_x
+            #print "min_y, max_y", min_y, max_y
+            a = (x > min_x and x < max_x)
+            b = (y > min_y and y < max_y)
+            c = (x <= max_pt_x and x >= min_pt_x)
+            d = (y <= max_pt_y and y >= min_pt_y)
+            #print "conditional", a, b
+            if (a or b) and (c and d):
+               return True
+            else:
+                return False
+
+    def gradient(self):
+        y_p = self.y1 - self.y0 
+        x_p = self.x1 - self.x0 
+        if x_p == 0:
+            return self.y1, 'inf'
+        elif y_p == 0:
+            return self.x1, 0
+        else:
+            return None, y_p/x_p
+
+    def y_intercept(self):
+        if self.constant:
+            if self.m == 'inf':
+                return None
+            if self.m == 0:
+                return self.x1
+        else:
+            return self.y1 - self.m * self.x1
+
+    def compare(self, line):
+        return (self.m == line.m and self.b == line.b and \
+            self.constant == line.constant)
+
+    def intersect(self, all_lines):
+        for line in all_lines:
+            intersect_in_range = self.line_intersection(line)
+            if intersect_in_range:
+                return True
+        return False
+
+    def __str__(self):
+        if self.constant:
+            if self.m == 'inf':
+                return "x = " + str(self.constant)
+            else:
+                return "y = " + str(self.constant)
+        else:
+            return "y = " + str(self.m) + "x + " + str(self.b)
+
 class Obstacle(object):
-    def __init__(self, xy):
-        print 'new obstacle'
+    def __init__(self, xy, grow=True):
         self.coords = self._format(xy)
-        self.coords_grown = self._grow()
-        self.lines = self.get_.lines()
-    
-    def get_lines(self):
+        if grow:
+            self.coords_grown = self._grow()
+            self.lines = self.obstacle_lines(self.coords_grown)
+            self.lines.extend(self.obstacle_lines(self.coords))
+        else:
+            self.coords_grown = self.coords[:]
+            self.lines = self.obstacle_lines(self.coords_grown)        
+
+    def obstacle_lines(self, A):
         lines = []
-        for i in range(len(self.coords_grown)-1):
-            curr_x, curr_y = self.coords_grown[i]
-            next_x, next_y = self.coords_grown[i+1]
-            m = (next_y - curr_y)/(next_x - curr_x)*1.0
-            b = curr_y - m * curr_x
-            lines.append([m, b])   
-        
-        curr_x, curr_y = self.coords_grown[-1]
-        next_x, next_y = self.coords_grown[0]
-        m = (next_y - curr_y)/(next_x - curr_x)*1.0
-        b = curr_y - m * curr_x
-        lines.append([m, b])
+        for i in range(len(A)-1):
+            curr_x, curr_y = A[i]
+            next_x, next_y = A[i+1]
+            lines.append(Line([curr_x, curr_y], [next_x, next_y]))
+        curr_x, curr_y = A[-1]
+        next_x, next_y = A[0]
+        lines.append(Line([curr_x, curr_y], [next_x, next_y]))
         return lines
         
     def __repr__(self):
@@ -35,7 +131,7 @@ class Obstacle(object):
             coords.append(map(float, xy[i]))
         return coords
     
-    def _grow(self, square=0.35):
+    def _grow(self, square=0.4):
         coords = []
         for x,y in self.coords:
             coords.append([x+square, y+square])
@@ -69,7 +165,6 @@ class Obstacle(object):
         img = np.zeros(size)
         for i in range(len(coords)-1):
             pt1, pt2 = pixel_location(coords[i], coords[i+1])
-            print pt1, pt2
             cv2.line(img, pt1, pt2, color, thickness)
         pt1, pt2 = pixel_location(coords[0], coords[-1])
         cv2.line(img, pt1, pt2, color, thickness)
--- src/world.py
+++ src/world.py
@@ -1,6 +1,32 @@
 from obstacle import Obstacle
+from obstacle import Line
 import numpy as np
 import cv2
+from math import sqrt
+
+
+class Graph():
+    def __init__(self):
+        self.nodes = {}
+
+    def node_exists(self, a):
+        return a in self.nodes
+
+    def add_node(self, a):
+        if not self.node_exists(a):
+            self.nodes[a] = {}
+
+    def add_edge(self, a, b, weight):
+        if not self.node_exists(a):
+            self.add_node(a)
+        if not self.node_exists(b):
+            self.add_node(b)
+        self.nodes[a][b] = weight
+        self.nodes[b][a] = weight
+
+    def shortest_path(self, source):
+        pass
+
 
 class World(object):
 
@@ -9,7 +35,68 @@ class World(object):
         self.goals = []
         self.populate_obstacles(obstacle_txt)
         self.populate_goal(goal_txt)
+        self.info, self.graph, self.all_lines = self.intialize_graph()
+        self.make_edges()
 
+    def intialize_graph(self):
+        count = 0
+        graph = Graph()
+        info = {}
+        lines = []
+        graph.add_node('start')
+        info['start'] = self.goals[0]
+
+        for j,obs in enumerate(self.obstacles[1:]):
+        #for j,obs in enumerate(self.obstacles):
+            for i in range(len(obs.coords_grown)):
+                key = str(j) + "_" +str(i)
+                graph.add_node(key)
+                info[key] = obs.coords_grown[i]
+            lines.extend(obs.lines)
+        graph.add_node('end')
+        info['end'] = self.goals[1]
+        lines.extend(self.obstacles[0].lines)
+        return info, graph, lines
+
+    def make_edges(self):
+        nodes = list(self.graph.nodes.keys())
+        n_nodes = len(nodes)
+
+        for i in range(n_nodes - 1):
+            curr_node = nodes[i]
+            for j in range(i + 1, n_nodes):
+                next_node = nodes[j]
+                #print str(curr_node) + " -> " + str(next_node)
+                curr_line = Line(self.info[curr_node], self.info[next_node])
+                obs_n = next_node[0]
+                obs_l = curr_node[0]
+
+                ret = curr_line.intersect(self.all_lines)
+                #print ret
+                #print ""
+                if not ret:
+                    x0, y0 = self.info[curr_node]
+                    x1, y1 = self.info[next_node]
+                    weight = np.sqrt( (x0-x1)**2 + (y0 - y1)**2 )
+                    self.graph.add_edge(curr_node, next_node, weight)
+        
+        # for i in range(n_nodes - 1):
+        #     curr_node = nodes[i]
+        #     for next_node in ['1_2', '1_1', '1_0']:
+        #         #next_node = nodes[j]
+        #         print str(curr_node) + " -> " + str(next_node)
+        #         curr_line = Line(self.info[curr_node], self.info[next_node])
+        #         obs_n = next_node[0]
+        #         obs_l = curr_node[0]
+
+        #         ret = curr_line.intersect(self.all_lines)
+        #         print ret
+        #         print ""
+        #         if not ret:
+        #             self.graph.add_edge(curr_node, next_node, 1.0)
+        
+
+        print self.graph.nodes
     def populate_obstacles(self, txt_file):
         with open(txt_file, 'r') as input_file:
             n_lines = int(next(input_file))
@@ -18,7 +105,10 @@ class World(object):
                 coords = []
                 for j in range(n_vertex):
                     coords.append(next(input_file).replace(' \r\n', '').split(' '))
-                self.obstacles.append(Obstacle(coords))
+                if i==0:
+                    self.obstacles.append(Obstacle(coords, grow=False))
+                else:
+                    self.obstacles.append(Obstacle(coords, grow=True))
 
     def populate_goal(self, goal_txt):
         f = open(goal_txt, 'r')
@@ -29,16 +119,33 @@ class World(object):
         self.goals.append(end)
     
     def draw(self,grown=False, size=(600, 900, 3)):
+        def pixel_location(curr_o):
+            curr_obs = np.array(curr_o) * -500/11.0 + np.array([275, 575])
+            x0, y0 = map(int, curr_obs)
+            return (y0, x0)
+        
         img = np.zeros(size)
         thickness = 2
         
         for i, obstacle in enumerate(self.obstacles):
             if i > 0:
                 thickness = 1
-                print 'obstacle' + str(i)
                 img += obstacle.draw(grown, size,thickness, color=(0, 255, 0))
             img += obstacle.draw(False, size,thickness)
-        
+
+        start =  pixel_location(self.goals[0])
+        end =  pixel_location(self.goals[1])
+        cv2.circle(img, start, radius = 5, color=(0,0,255)) 
+        cv2.circle(img, end, radius = 5, color=(0,255,0)) 
+
+        for key in self.graph.nodes:
+            curr = self.info[key]
+            start = pixel_location(curr)
+            for key2 in self.graph.nodes[key]:
+                next = self.info[key2]
+                end = pixel_location(next)
+                cv2.line(img, start, end, color=(255,0,0), thickness=1)
+
         cv2.imshow('world', img)
         cv2.waitKey(0)
 
@@ -51,4 +158,5 @@ class World(object):
 
 if __name__ == '__main__':
     W = World('../input_files/hw4_world_and_obstacles_convex.txt', '../input_files/hw4_start_goal.txt')
+    #W = World('../input_files/small_world.txt', '../input_files/hw4_start_goal.txt')
     W.draw(grown=True)
