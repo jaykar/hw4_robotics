1c3cd76fdf8b8483b2c650e5fbb8399c9639dbf6
as <arthi@dyn-129-236-212-79.dyn.columbia.edu>
2015-11-18 17:39:00 -0500

    new convex hull

--- src/find_convex_hull.py
+++ src/find_convex_hull.py
@@ -43,24 +43,31 @@ def find_convex_hull(pts_array):
         P_i = sorted_pts[i][2];
         c = pt_stack.pop();
         d = pt_stack.pop();
-        print(P_i, c, d)
         pt_stack.append(d);
         pt_stack.append(c);
         # find the line formed by these two points and see if the point Pi is
         # strictly to the left of this line
         is_to_the_left = False
         if c[0] != d[0]: # not a vertical line in conventional xy plane
-            m = (c[1] - d[1])/(c[0] - d[0])
-            b = c[1] - m*(c[0])
-            print(m, b)
-            if m == 0: 
-                if (P_i[0] < min(c[0], d[0])):
-                    is_to_the_left = True
-            else:
-                x_line = (P_i[1] - b)/m
-                if P_i[0] < x_line:
-                    is_to_the_left = True
-        else: 
+#            m = (c[1] - d[1])/(c[0] - d[0])
+#            b = c[1] - m*(c[0])
+#            print(c, d)
+#            print(m, b)
+#            print(P_i)
+#            if m == 0.0:
+#                print('Case a')
+#                if (P_i[0] = min(c[0], d[0])):
+#                    is_to_the_left = True
+#            else:
+#                print('Case b')
+#                x_line = (P_i[1] - b)/m
+#                if P_i[0] < x_line:
+#                    is_to_the_left = True
+            position = (d[0] - c[0]) * (P_i[1] - c[1]) - (d[1] - c[1]) * (P_i[0] - c[0])
+            if position < 0:
+                is_to_the_left = True
+        else:
+            print('Case c')
             if P_i[0] < c[0]:
                 is_to_the_left = True
         print(is_to_the_left)
@@ -73,8 +80,8 @@ def find_convex_hull(pts_array):
         print(pt_stack)
         print('\n')
     return pt_stack;
-
-pts = [[0, 1], [1, 5], [2, 3], [2, 3], [3, 5], [3, 2], [4, 2], [6, 3]];                      
+#pts = [[0.0, 1.0], [1.0, 5.0], [2.0, 3.0], [2.0, 3.0], [3.0, 5.0], [3.0, 2.0], [4.0, 2.0], [6.0, 3.0]];
+pts = [[-0.111, -1.374], [-0.111, -2.174], [-0.911, -1.374], [-0.911, -2.174], [-0.111, -1.843], [-0.111, -2.643], [-0.911, -1.843], [-0.911, -2.643], [0.699, -1.843], [0.699, -2.643], [-0.101, -1.843], [-0.101, -2.643], [0.699, -1.374], [0.699, -2.174], [-0.101, -1.374], [-0.101, -2.174]]                    
 hull = find_convex_hull(pts);      
 print(hull)       
          
